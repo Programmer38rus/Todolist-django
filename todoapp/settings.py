27f9c27acb950dd6b6b16874455def5009fcc450
im83 <programmer38rus@gmail.com>
2020-12-11 19:08:25 +0800

    deploy

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'please-set-secret-key-through-env')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'second.slavakorotkin.ru']
 
 
 INSTALLED_APPS = [
